101d6ee25f44ad83a1ee1b4e91d1aa438c39a1c1
Molly Graham <mollyclaire.graham@gmail.com>
2019-08-31 15:50:35 -0500

    Added onChange and onSubmit methods to form. Installed Redux.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Post from "./components/Post";
+import PostForm from "./components/PostForm";
+import { Provider } from "react-redux";
+import { createStore, applyMiddlware } from "redux";
+
+const store = createStore(() => [], {}, applyMiddlware());
 
 function App() {
   return (
-    <div className="App">
-      <Post />
-    </div>
+    // The store holds the state tree inside of our application 
+    <Provider store={store}>
+      <div className="App">
+        <PostForm />
+        <Post />
+      </div>
+    </Provider>
   );
 }
 
--- my-app/src/components/Post.js
+++ my-app/src/components/Post.js
@@ -1,9 +1,33 @@
 import React from "react";
 
 class Post extends React.Component {
+constructor(props) {
+    super(props)
+    this.state= {
+        posts: []
+    }
+}
+
+    componentDidMount() {
+        fetch("https://jsonplaceholder.typicode.com/posts")
+        // This promise returns the response in json form
+        .then (res => res.json())
+        .then(data => this.setState({posts: data}))
+    }
+
     render() {
+        // Note that the variable declared here is return later as a JSX object (Note to self: review this with Everyday Art app)
+        const postItems = this.state.posts.map(post => (
+            <div key={post.id}>
+                <h3>{post.title}</h3>
+                <p>{post.body}</p>
+            </div>
+        ))
         return (
-            <div>Post</div>
+            <div>
+                <h1>Posts</h1>
+                {postItems}
+            </div>
         )
     }
 }
