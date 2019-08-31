c342ef64e6d7a5ad2d428436bf766d1e065462dd
Molly Graham <mollyclaire.graham@gmail.com>
2019-08-31 16:11:18 -0500

    Got reducer set up and also added actions folder.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,13 +3,12 @@ import './App.css';
 import Post from "./components/Post";
 import PostForm from "./components/PostForm";
 import { Provider } from "react-redux";
-import { createStore, applyMiddlware } from "redux";
-
-const store = createStore(() => [], {}, applyMiddlware());
+import store from "./store";
 
 function App() {
   return (
     // The store holds the state tree inside of our application 
+    // The Provider should wrap the application
     <Provider store={store}>
       <div className="App">
         <PostForm />
---
+++ my-app/src/reducers/postReducer.js
@@ -0,0 +1,21 @@
+// This file evaluates any actions that are committed (such as fetching posts
+// and creating posts. For our actions, we create types -- see imported file below)
+
+import { FETCH_POSTS, NEW_POST } from "../actions/types";
+
+// In the postReducer, I want to create the initial state (which is an object)
+const initialState = {
+    // These items represent the posts that come in from the action (the fetch request)
+    items: [],
+    // This item represents the single post that we add
+    item: {}
+}
+
+// This function evaluates what type we are dealing with. It takes in two things: the state and the action.
+// The action will include a type.
+export default function(state = initialState, action) {
+    switch(action.type) {
+        default:
+            return state;
+    } 
+}
